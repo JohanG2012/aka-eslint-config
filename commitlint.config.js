b9a4a948dd258bb5daa7a84dbf99b0afee8f7009
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2023-11-02 18:47:51 +0100

    config: add more keywords

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,25 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          ...require('@commitlint/config-conventional').rules['type-enum'][2],
+          // Your custom types
+          'init',
+          'config',
+          'change',
+          'major',
+          'minor',
+          'patch',
+          'release-candidate',
+          'tests',
+          'style',
+          'lint-fix',
+          'docs',
+          'version-bump'
+        ],
+      ],
+    },
   };
-  
